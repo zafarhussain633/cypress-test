b7c9fe3773f7a0bae30ca24aa84b6f601352e78d
details.wes <details.wes@gmail.com>
2023-04-07 14:07:58 +0530

    added cypress

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,21 @@
+// describe('My First Test', () => {
+//   it('clicks the link "type"', () => {
+//     cy.visit('https://example.cypress.io')
+
+//     cy.contains('type').click()
+//   })
+// })
+
+describe('My First Test', () => {
+  it('clicking "type" navigates to a new url', () => {
+    cy.visit('https://example.cypress.io')
+
+    cy.contains('type').click()
+
+    // Should be on a new URL which
+    // includes '/commands/actions'
+    cy.url().should('include', '/commands/actions')
+  })
+})
+
+
